46b3995bc61cd3fb3d27f0b2dc87fe8bc5cc12de
janusz.sidorczuk <janusz.sidorczuk@gmail.com>
2020-07-23 15:41:26 +0200

    startproject

---
+++ src/main/java/model/EntityBasic.java
@@ -0,0 +1,53 @@
+package model;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table( name = "EVENTS" )
+public class EntityBasic {
+    private Long id;
+
+    private String title;
+    private Date date;
+
+    public EntityBasic( ) {
+        // this form used by Hibernate
+    }
+
+    public EntityBasic(String title, Date date) {
+        // for application use, to create new events
+        this.title = title;
+        this.date = date;
+    }
+
+    @Id
+    @GeneratedValue(generator="increment")
+    @GenericGenerator(name="increment", strategy = "increment")
+    public Long getId() {
+        return id;
+    }
+
+    private void setId(Long id) {
+        this.id = id;
+    }
+
+    @Column(name = "EVENT_DATE")
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
